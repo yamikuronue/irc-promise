ee305ede978930e89c54e12da1e89195db8e2963
Yami <yamikuronue@gmail.com>
2017-04-15 23:53:42 +0000

    feature: it now connects successfully! (more tests coming soon)

--- src/connection.js
+++ src/connection.js
@@ -2,46 +2,66 @@
  
 const debug = require('debug')('sockbot:providers:irc:connection');
 const tls = require('tls');
+const net = require('net');
 const EventEmitter = require('events').EventEmitter;
+const os = require('os');
 
 class Connection extends EventEmitter {
     constructor(options) {
         super();
         this.data = {
             server: options.server,
-            port: options.port
+            port: options.port,
+            nickname: options.nick
         };
+        
+        this.buffer = new Buffer('');
     }
     
     connect() {
         let data = this.data;
         let self = this;
         return new Promise(function(resolve, reject) {
-            data.con = tls.connect({
+           // data.con = tls.connect({
+            data.con = net.connect({
                 host: data.server,
                 port: data.port
-            }, function(response) {
-                debug(response);
-                resolve();
-            });
-
-            /*istanbul ignore next*/
-            data.con.addListener('data', function(data) {
-                const line = data.toString('utf8');
-                self.parseData(line);
-            });
+            }, () => self._phase2(resolve));
+            
+            //Start the data processing
+            data.con.on('data', (data) => self.handleData(data));
         });
     }
     
+    handleData(data) {
+        let chunk = data.toString('utf8');
+        this.buffer += chunk;
+        
+        let lines = this.buffer.split('\r\n');
+        this.buffer = lines.pop();                  //Anything that doesn't end in a linebreak goes back on the buffer
+        lines.map((line) => this.parseData(line));
+    }
+    
+    send(line) {
+        debug(`<- ${line}`);
+        this.data.con.write(line + '\r\n');
+    }
+    
+    _phase2(resolve) {
+        this.send(`NICK ${this.data.nickname}`);
+        this.send(`USER ${this.data.nickname} 0 * :Powered by SockDrawer`);
+        resolve();
+    }
+    
     parseData(line) {
-        const match = /:([^ ]+) ([A-Za-z]+) (.+)?/.exec(line);
-        debug(line)
+        debug(` -> ${line}`);
+        const match = /(:[^ ]+ )?([A-Za-z0-9]+) (.+)?/.exec(line);
         if (match) {
             const msg = {
                 prefix: match[1], //prefix comes up to the first space
                 command: match[2], //then the command
-                args: match[3] //everything else is arguments
-            }
+                args: this.parseArgs(match[3]) //everything else is arguments
+            };
 
             this.emit("data", msg);
             
@@ -50,14 +70,47 @@ class Connection extends EventEmitter {
                 case 'NOTICE':
                     this.parseNotice(msg);
                     break;
+                case 'PING':
+                    this.replyPing(msg);
+                    break;
+                case '001':
+                    this.autoJoin(msg);
             }
         } else {
             debug('Data failed to parse: ' + line);
         }
     }
     
+    parseArgs(argString) {
+        let ret = [];
+        let temp = '';
+        let colon = false;
+        
+        argString.split(' ').map((val, index) => {
+            //Any arguments starting at position 15 are treated as one argument
+            if (val.startsWith(':')) colon = true;
+            
+            if (index >= 14 || colon) {
+                temp += val + ' ';
+            } else {
+                ret.push(val);
+            }
+        });
+        
+        if (temp) {
+            temp = temp.trim();
+            ret.push(temp);
+        }
+        
+        return ret;
+    }
+
+    autoJoin(data) {
+        //Join channels
+    }
+    
     replyPing(data) {
-        this.data.con.write("PONG"); //reply to keep connection alive
+        this.send(`PONG ${data.args}`); //reply to keep connection alive
         this.emit("ping", data); //in case anyone cares;
     }
     
--- test/connectionTest.js
+++ test/connectionTest.js
@@ -10,26 +10,32 @@ chai.use(require('sinon-chai'));
 chai.should();
 
 const Sinon = require('sinon');
+const fakeTLS = {
+    addListener: Sinon.stub(),
+    write: Sinon.stub()
+};
 
 describe('Connection', () => {
         var opts = {
             server: 'irc.darkmyst.org',
-            port: '6697'
+            port: '6697',
+            nick: 'aNickName'
         };
     
     let sandbox, con;
-    
-    
-    
+
     beforeEach(function() {
         sandbox = Sinon.sandbox.create();
         con = new Connection(opts);
         sandbox.stub(con, "emit");
         sandbox.stub(require('tls'), "connect").yields();
+        con.data.con = fakeTLS;
     });
     
     afterEach(function() {
         sandbox.restore();
+        fakeTLS.write.reset();
+        fakeTLS.addListener.reset();
     });
     
     it('exists', () => {
@@ -39,18 +45,25 @@ describe('Connection', () => {
     it('connects to servers', () => {
         return con.connect().should.resolve;
     });
+
+    it('sends a NICK', () => {
+        const fakeResolve = Sinon.stub();
+        con._phase2(fakeResolve);
+        fakeTLS.write.should.have.been.calledWith('NICK aNickName\r\n');
+        fakeResolve.should.have.been.called;
+    });
+    
+    it('sends a USER', () => {
+        const fakeResolve = Sinon.stub();
+        con._phase2(fakeResolve);
+        fakeTLS.write.should.have.been.calledWith(Sinon.match('USER'));
+        fakeResolve.should.have.been.called;
+    });
     
     describe('replyPing', () => {
         it('should reply "PONG"', () => {
-            let con = new Connection(opts);
-            const fakeTLS = {
-                write: Sinon.stub()
-            };
-            
-            con.data.con = fakeTLS;
-            
-            con.replyPing({});
-            return fakeTLS.write.should.have.been.calledWith('PONG');
+            con.replyPing({args: '12345'});
+            return fakeTLS.write.should.have.been.calledWith('PONG 12345\r\n');
         });
     });
     
@@ -59,15 +72,20 @@ describe('Connection', () => {
             con.parseData(":prometheus.no.eu.darkmyst.org NOTICE * :*** Looking up your hostname...");
             return con.emit.should.have.been.calledWith('data');
         });
-        it('should parse another valid line', () => {
+
+        it('should parse two lines', () => {
+            sandbox.stub(con, 'parseNotice');
+            con.parseData(":prometheus.no.eu.darkmyst.org NOTICE * :*** Looking up your hostname...");
             con.parseData(":flame.de.eu.darkmyst.org NOTICE * :*** Found your hostname");
-            return con.emit.should.have.been.calledWith('data');
+            return con.emit.should.have.been.calledTwice;
         });
-        
-        it('should reject a non-line', () => {
-            con.parseData("Pollen Warning!");
-            return con.emit.should.not.have.been.called;
-        });
+    });
+    
+    describe('parseArgs', () => {
+        it('should separate on spaces', () => con.parseArgs('some args').should.deep.equal(['some', 'args']));
+        it('should stop at 15', () => con.parseArgs('1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18')
+            .should.deep.equal(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15 16 17 18']));
+        it('should treat : special', () => con.parseArgs('one two :colon and then some words').should.deep.equal(['one', 'two', ':colon and then some words']));
     });
 
     describe('parseNotice', () => {
@@ -77,7 +95,6 @@ describe('Connection', () => {
         });
         
         it('should reject a non-notice', () => {
-            
             con.parseNotice({"prefix":"maple.on.ca.darkmyst.org","command":"NOTICE","args":"aaaaaaaaaaaaaaaa"});
             return con.emit.should.not.have.been.calledWith('NOTICE');
         });
